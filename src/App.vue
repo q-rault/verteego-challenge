369a7b628286942c9d40b48f1a70ff92401dbfa2
Quentin Rault <q.rault054@gmail.com>
2022-02-11 15:57:52 +0100

    first commit with initial display using create-vue and verteego theme colors

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<script setup>
+import IpViewerPage from "./components/IpViewerPage.vue";
+</script>
+
+<template>
+  <header id="header">
+    <div class="logo-container">
+      <img
+        alt="Verteego short logo"
+        class="logo"
+        src="./assets/circle-logo.svg"
+        width="37"
+        height="37"
+      />
+    </div>
+  </header>
+  <IpViewerPage />
+  <footer>
+    <span>Développé par Quentin Rault avec </span>
+    <img alt="Vue logo" class="inline-image" src="./assets/vue-logo.svg" />
+    <span>ue.js</span>
+  </footer>
+</template>
+
+<style>
+@import "./assets/base.css";
+
+#app {
+  margin: 0;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+
+#header {
+  width: 100%;
+  background-color: var(--color-heading);
+  color: var(--color-background);
+}
+
+.logo-container {
+  max-width: fit-content;
+  max-height: fit-content;
+}
+
+.logo {
+  display: block;
+  margin: 0.5rem;
+}
+
+footer {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  min-height: 50px;
+  margin-top: auto;
+  background-color: var(--color-heading);
+  color: var(--color-background);
+}
+
+.inline-image {
+  display: inline-block;
+  width: 1rem;
+  height: 1rem;
+  margin-left: 0.25rem;
+}
+</style>
---
+++ src/components/IpViewerPage.vue
@@ -0,0 +1,24 @@
+<script setup>
+defineProps({
+  ip: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="page-container">
+    <h1>Verteego challenge</h1>
+  </div>
+</template>
+
+<style scoped>
+.page-container {
+  display: flex;
+  flex-direction: column;
+  place-items: center;
+  margin: 0 3rem;
+  padding: 1rem 0;
+}
+</style>
