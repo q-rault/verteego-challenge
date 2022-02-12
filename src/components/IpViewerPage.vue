4441dccb5fb5b3dc55484c89b17cff203c6d3ec6
Quentin Rault <q.rault054@gmail.com>
2022-02-12 12:07:08 +0100

    improved layout with CSS

--- src/components/IpViewerPage.vue
+++ src/components/IpViewerPage.vue
@@ -4,7 +4,7 @@ import { onMounted, reactive, computed } from "vue";
 const state = reactive({ ipAddress: "loading", isIpV4: true });
 
 const ipV4 = "IPv4";
-const ipV6 = "Universal: IPv4/IPv6";
+const ipV6 = "IPv4/IPv6";
 
 const currentIpVersion = computed(() => {
   return state.isIpV4 ? ipV4 : ipV6;
@@ -50,7 +50,8 @@ onMounted(() => {
   <div class="page-container">
     <h1>Verteego challenge</h1>
     <p>This is your ip address :</p>
-    <p>{{ state.ipAddress }} ({{ currentIpVersion }})</p>
+    <p>{{ state.ipAddress }}</p>
+    <p>({{ currentIpVersion }})</p>
     <button @click.prevent="handleClick">Switch to {{ nextIpVersion }}</button>
   </div>
 </template>
@@ -62,5 +63,32 @@ onMounted(() => {
   place-items: center;
   /* margin: 0 3rem; */
   padding: 1rem 0;
+}
+
+h1 {
+  margin: 2rem 0;
+}
+
+p {
+  margin-bottom: 1rem;
+}
+
+button {
+  margin: 1rem;
+  border-radius: 0.25rem;
+  background-color: var(--color-logo); /* Green */
+  border: none;
+  color: var(--color-background);
+  padding: 15px 32px;
+  width: 100%;
+  transition-duration: 0.5s;
+}
+button:hover {
+  transform: scale(1.05);
+}
+button:active {
+  opacity: 0.8;
+  transform: scale(0.95);
+  transition-duration: 0.3s;
 }
 </style>
